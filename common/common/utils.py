82d0442c9a7bdad2cc3a97ef9cabcd0a7e255533
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2026-06-19 17:22:38 +0200

    Fix password generation in tests

--- common/common/utils.py
+++ common/common/utils.py
@@ -10,4 +10,13 @@ import string
 def generate_password() -> str:
     """Generate a random password."""
     choices = string.ascii_letters + string.digits
-    return "".join(secrets.choice(choices) for _ in range(24))
+    # Might seem risky but in fact the probability that a password doesn't pass these checks is low
+    while True:
+        password = "".join([secrets.choice(choices) for i in range(24)])
+        # These checks are consistent with the rules for the password validation on MySQL server
+        if all((
+            any(c.islower() for c in password),
+            any(c.isupper() for c in password),
+            any(c.isdigit() for c in password),
+        )):
+            return password
